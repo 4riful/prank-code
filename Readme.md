9fe9104519e64ac06c7328e516fb6bd27f186a9e
Ariful Anik <45453772+4riful@users.noreply.github.com>
2018-11-30 02:42:10 +0600

    Prank
    This is such #prank file .not fear about
    It can not do any harm to you
    #xd

---
+++ Readme.md
@@ -0,0 +1,6 @@
+*Author:Ariful Anik,plscr
+it's excutable with python!
+#####$$$#$#########
+Don't fear ..
+it's just a prank it can't do any harm to your device####
+let's enjoy ####xd 
---
+++ prank.py
@@ -0,0 +1,19 @@
+name=input("what's your name ?? name:::::")
+x=name.title()
+print(f"Your name is {x} ")
+y=len(x)
+print(f"length of your name is {y}")
+print(f"hahhaha reverse of your name is {x[-1::-1]}")
+age =input("whats your age?:::::: ")
+age=int(age)
+if age>=18:
+	print("you are 18+ bro.......")
+else:
+	print("you are kid ....take horlicks")
+x=input('will you believe me?? yes/no   ::')
+if x=='no':
+	while True:
+		print("you are hacked")
+		print("your system is cracked")
+elif x=='yes':
+	print('you are safe dude ...wanna friend with me ??then my fb profile is ✓✓✓✓✓https://m.me/nightstar.anik')
