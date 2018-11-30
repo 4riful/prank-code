19d8ddf642aa9daf4dfec0425241874262864e93
Ariful Anik <45453772+4riful@users.noreply.github.com>
2018-11-30 11:34:22 +0600

    Updated prank.py

--- prank.py
+++ prank.py
@@ -1,19 +1,21 @@
-name=input("what's your name ?? name:::::")
+name=input("what's your name ??\n name:::::")
 x=name.title()
-print(f"Your name is {x} ")
+print(f"#1.Your name is \nMR\t{x} ")
+print('_______________________')
 y=len(x)
-print(f"length of your name is {y}")
-print(f"hahhaha reverse of your name is {x[-1::-1]}")
-age =input("whats your age?:::::: ")
+print(f"#2.length of your name is {y}")
+print("#########################")
+print(f"#3.hahhaha can you imagine\n reverse of your name is {x[-1::-1]}")
+age =input("whats your age? \n==> ")
 age=int(age)
 if age>=18:
 	print("you are 18+ bro.......")
 else:
-	print("you are kid ....take horlicks")
-x=input('will you believe me?? yes/no   ::')
+	print("you are kid ..\n..take horlicks")
+x=input('will you believe me?? \nyes/no   ::>')
 if x=='no':
 	while True:
 		print("you are hacked")
 		print("your system is cracked")
 elif x=='yes':
-	print('you are safe dude ...wanna friend with me ??then my fb profile is ✓✓✓✓✓https://m.me/nightstar.anik')
+	print('you are safe dude .\n..wanna friend with me ??\nthen my fb profile is=> ✓✓✓✓✓https://m.me/nightstar.anik')
