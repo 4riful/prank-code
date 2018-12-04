ab595a3df5e600db6ffa3de223596542aecb9f58
Ariful Anik <45453772+4riful@users.noreply.github.com>
2018-12-05 00:17:27 +0600

    Updated prank.py

--- prank.py
+++ prank.py
@@ -1,21 +1,23 @@
-name=input("what's your name ??\n name:::::")
+name=input("what's your name ??\n ➡️ ➡️ ➡️")
 x=name.title()
 print(f"#1.Your name is \nMR\t{x} ")
 print('_______________________')
 y=len(x)
+print(" 🐲 🐲 🐲 🐲 🐲 🐲 🐲 ")
 print(f"#2.length of your name is {y}")
 print("#########################")
 print(f"#3.hahhaha can you imagine\n reverse of your name is {x[-1::-1]}")
 age =input("whats your age? \n==> ")
 age=int(age)
 if age>=18:
-	print("you are 18+ bro.......")
+	print("you are 18+ bro 😎 😎 😎")
 else:
-	print("you are kid ..\n..take horlicks")
+	print("you are kid ..\n..take horlicks 👺 👺 👺 👺 👺 👺")
+print('Don\'t believe a stranger 👽 👽 👽 👽 👉🏻 👉🏻 ')
 x=input('will you believe me?? \nyes/no   ::>')
 if x=='no':
 	while True:
-		print("you are hacked")
-		print("your system is cracked")
+		print("you are hacked 🐲 🕥 🐲")
+		print("your system is cracked 🐲 🐲 🐲")
 elif x=='yes':
 	print('you are safe dude .\n..wanna friend with me ??\nthen my fb profile is=> ✓✓✓✓✓https://m.me/nightstar.anik')
